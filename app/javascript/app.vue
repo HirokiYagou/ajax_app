49621f23b08b0746c312c10070c4158b19f6056a
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-10 22:03:23 +0900

    vue3導入

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  background-color: azure;
+  font-size: 2em;
+  text-align: left;
+}
+</style>
