b350f237dd5a35f6d0609960e4f13aa64b2b8a2f
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-11 15:55:03 +0900

    コントローラーを公式SDK通りに修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,7 +1,7 @@
 class LinebotController < ApplicationController
   require 'line/bot'
 
-  protect_from_fogery with: :exception
+  protect_from_fogery :except => [:callback]
 
   def client
     @client ||= Line::Bot::Client.new { |config|
@@ -15,12 +15,11 @@ class LinebotController < ApplicationController
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :bad_request
+      error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
-
-    events.each { |event|
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -29,10 +28,15 @@ class LinebotController < ApplicationController
             type: 'text',
             text: event.message['text']
           }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
         end
       end
-    }
-
-    head :ok
+    end
+    
+    "OK"
   end
 end
