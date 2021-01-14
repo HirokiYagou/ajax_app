3ac5994abda421e840e10de1d190aeabb6f9da9a
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-14 15:38:16 +0900

    Revert "Update environment.js"
    This reverts commit cc1ce127dc6e320eba1893de35acb63b16e72568.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,33 +1,31 @@
 const { environment } = require('@rails/webpacker')
-const path = require('path')
-const { DefinePlugin } = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue');
+const vue = require('./loaders/vue')
 
-const customConfig = {
-  resolve:{
-    alias: {
-      '@': path.resolve(__dirname, '..', '..', 'app/javascript/src')
-    }
-  }
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
 }
 
-environment.config.merge(customConfig)
-
-environment.plugins.prepend(
-    'VueLoaderPlugin',
-    new VueLoaderPlugin()
-)
-
-environment.plugins.prepend(
-  'Define',
-  new DefinePlugin({
-      __VUE_OPTIONS_API__: false,
-      __VUE_PROD_DEVTOOLS__: false
-  })
-)
+module.exports = environment
 
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+module.exports = environment
+
+const customConfig = require('./custom')
+environment.config.merge(customConfig)
 
 module.exports = environment
